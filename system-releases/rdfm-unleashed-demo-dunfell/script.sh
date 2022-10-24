71b139d91584ff69285dcd7ed3b09a82e548b40c
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-10-24 13:19:36 +0200

    [#37877] system-releases: rdfm-unleashed-demo-dunfell: script.sh: Add 'rdfm-image-upgraded' to the building process

--- system-releases/rdfm-unleashed-demo-dunfell/script.sh
+++ system-releases/rdfm-unleashed-demo-dunfell/script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-PARALLEL_MAKE="-j $(nproc)" BB_NUMBER_THREADS="$(nproc)" MACHINE="freedom-u540" bitbake core-image-minimal $@
+PARALLEL_MAKE="-j $(nproc)" BB_NUMBER_THREADS="$(nproc)" MACHINE="freedom-u540" bitbake rdfm-image-minimal $@
+PARALLEL_MAKE="-j $(nproc)" BB_NUMBER_THREADS="$(nproc)" MACHINE="freedom-u540" bitbake rdfm-image-upgraded $@
