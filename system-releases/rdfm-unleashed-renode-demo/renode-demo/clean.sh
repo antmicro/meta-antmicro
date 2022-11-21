e0213a08c24bbb38aeb7a1f32701ed25ff896a55
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-11-21 12:15:16 +0100

    [#37880] system-releases: rdfm-unleashed-renode-demo: renode-demo: clean.sh: Add minimal rdfm image

--- system-releases/rdfm-unleashed-renode-demo/renode-demo/clean.sh
+++ system-releases/rdfm-unleashed-renode-demo/renode-demo/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -r volume/rdfm-image-minimal-freedom-u540.flash.sdimg volume/rdfm-image-upgraded-freedom-u540.rdfm volume/fw_payload.elf volume/log*
+rm -r volume/rdfm-image-minimal-freedom-u540.flash.sdimg volume/rdfm-image-minimal-freedom-u540.rdfm volume/rdfm-image-upgraded-freedom-u540.rdfm volume/fw_payload.elf volume/log*
