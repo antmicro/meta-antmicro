20d48b07edce638f77e912db039db4d9af326a01
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-11-21 12:13:24 +0100

    [#37880] system-releases: rdfm-unleashed-demo: script.sh: Remove building script

--- system-releases/rdfm-unleashed-demo/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-PARALLEL_MAKE="-j $(nproc)" BB_NUMBER_THREADS="$(nproc)" MACHINE="freedom-u540" bitbake rdfm-image-minimal $@
-PARALLEL_MAKE="-j $(nproc)" BB_NUMBER_THREADS="$(nproc)" MACHINE="freedom-u540" bitbake rdfm-image-upgraded $@
